2d5007e9df7824791276078c4332eb422e3282a2
Nandan Kumar Singh <nandan3512@gmail.com>
2022-06-19 08:42:41 +0530

    done

---
+++ sign up/sign_up.js
@@ -0,0 +1,20 @@
+let x= document.querySelector("#form");
+
+x.addEventListener("submit",reg);
+let arr=[];
+
+function reg(event){
+  event.preventDefault();
+
+  if(form.pword.value != form.cpword.value){
+    alert("Password and Confirm Password doesn't match.")
+  }else{
+    let obj={
+      email: form.email.value,
+      password: form.pword.value,
+    }
+    arr.push(obj);
+    localStorage.setItem("users",JSON.stringify(arr));
+    location.replace("/Web-19-IndianExpress/sign in/sign_in.html")
+  }
+}
