68e4b07c06473cbb7fbf926ee9a3d39a620c0858
Nandan Kumar Singh <nandan3512@gmail.com>
2022-06-19 16:22:55 +0530

    corrected

--- sign in/sign_in.js
+++ sign in/sign_in.js
@@ -7,14 +7,14 @@ function login(event){
   event.preventDefault();
   
   if(nusers == 0){
-    location.replace("/Web-19-IndianExpress/sign up/sign_up.html")
+    location.replace("../sign up/sign_up.html")
   }else{
     if(form.email.value != nusers[0].email){
       alert("Email is invalid.")
     }else if(form.pword.value != nusers[0].password){
       alert("Password is invalid.")
     }else{
-      location.replace("/Web-19-IndianExpress/premium/premium.html")
+      location.replace("../premium/premium.html")
     }
   }
   
